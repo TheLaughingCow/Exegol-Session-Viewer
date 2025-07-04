c5d0da43d4013a22777eee1a15a3f56d9fba5d5d
Frozenk <frozenk@frozenk.fr>
2025-07-04 14:55:26 +0200

    Create esw-launcher.py

---
+++ esw-launcher.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import os, sys, subprocess, urllib.request
+
+venv_path = os.path.expanduser("~/.venv/exegol-replay")
+python_path = os.path.join(venv_path, "bin", "python3")
+script_real = os.path.join(os.path.dirname(__file__), "exegolsessionsviewer.py")
+tty2img_path = os.path.join(os.path.dirname(__file__), "tty2img.py")
+tty2img_url = "https://raw.githubusercontent.com/opcode-eu-org-libs/asciicast2movie/master/tty2img.py"
+
+if not os.path.exists(venv_path):
+    print("[+] Creating virtual environment for Exegol Replay...")
+    subprocess.check_call([sys.executable, "-m", "venv", venv_path])
+
+pip = os.path.join(venv_path, "bin", "pip")
+subprocess.check_call([pip, "install", "--upgrade", "pip"])
+
+editor_py = os.path.join(venv_path, "lib", f"python{sys.version_info.major}.{sys.version_info.minor}", "site-packages", "moviepy", "editor.py")
+if not os.path.exists(editor_py):
+    print("[!] moviepy/editor.py missing, patching automatically for compatibility...")
+    with open(editor_py, "w") as f:
+        f.write("from moviepy import *\n")
+
+ 
+if not os.path.exists(tty2img_path):
+    print("[+] Downloading tty2img.py...")
+    urllib.request.urlretrieve(tty2img_url, tty2img_path)
+
+os.execv(python_path, [python_path, script_real] + sys.argv[1:])
