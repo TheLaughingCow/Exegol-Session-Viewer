8075491534b04d3d2f79e016a1cdd847833b073b
Frozenk <frozenk@frozenk.fr>
2025-07-04 15:34:44 +0200

    Update install.sh
    fixbug

--- install.sh
+++ install.sh
@@ -7,17 +7,18 @@ SCRIPT="esw-launcher.py"
 ALIAS="alias esw='python3 $TARGET/$SCRIPT'"
 
 echo "[+] Moving project to $TARGET..."
-sudo rm -rf "$TARGET"
-sudo mv . "$TARGET"
 
-# Detect shell config file
+cd "$(dirname "$0")/.."  
+sudo rm -rf "$TARGET"
+sudo mv Exegol-Session-Viewer "$TARGET"
+
+ 
 if [ -n "$ZSH_VERSION" ]; then
     RC="$HOME/.zshrc"
 else
     RC="$HOME/.bashrc"
 fi
-
-# Add alias if not present
+ 
 if ! grep -Fxq "$ALIAS" "$RC"; then
     echo "[+] Adding alias to $RC"
     echo "$ALIAS" >> "$RC"
